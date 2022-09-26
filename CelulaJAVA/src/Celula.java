9a4bb33245376140795b3b2fdd81004cf39a0cb8
MrAnthropocene <montesj4@uabc.edu.mx>
2022-09-25 23:03:34 -0700

    Codigos Java desde rama de Montes

---
+++ CelulaJAVA/src/Celula.java
@@ -0,0 +1,19 @@
+public class Celula {
+
+	private string TipoDeCelula;
+
+	public string getTipoDeCelula() {
+		// TODO - implement Celula.getTipoDeCelula
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param TipoDeCelula
+	 */
+	public void setTipoDeCelula(int TipoDeCelula) {
+		// TODO - implement Celula.setTipoDeCelula
+		throw new UnsupportedOperationException();
+	}
+
+}
